f29887cace995fcd37fa8d7e76c32a738c5425f8
thestevek <steve.kryworuka@automattic.com>
2022-09-22 15:39:32 -0400

    add pagination feature

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-elements'), 'version' => 'f193cf11344cd610da82');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-elements'), 'version' => 'd52efcace3d770cae57d');
